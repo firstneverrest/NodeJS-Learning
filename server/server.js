f77d1d412cc57d4367ab02d3fc3993ae032e81e2
Chitsanupong <first30952@gmail.com>
2021-10-03 11:40:29 +0700

    feat: client & server

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(4000, 'localhost', () => {
+  console.log('listening on port 4000');
+});
